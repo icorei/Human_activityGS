920c4cf7276326c04e99d038336bea679a81b0e2
jrfep <jr.ferrer.paris@gmail.com>
2020-04-03 09:45:00 +1100

    Script to run web of science searches through the clarivate APIs, #3

---
+++ R/bibliometric-analysis/step1_search_keywords_WOS.R
@@ -0,0 +1,5 @@
+## Source code to run web of science searches through the clarivate APIs
+## https://www.programmableweb.com/api/clarivate-web-science-expanded
+## https://clarivate.com/webofsciencegroup/solutions/xml-and-apis/
+
+## to do...
