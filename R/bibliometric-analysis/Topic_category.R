cdfca49929d46260598403bf4268e9df89f7f670
icorei <42251398+icorei@users.noreply.github.com>
2020-04-16 09:21:28 +0200

    New diccionary

---
+++ R/bibliometric-analysis/Topic_category.R
@@ -0,0 +1,88 @@
+%
+01 Protected_Areas
+02 Large_Mammals
+03 Density_Estimation
+04 Occupancy_model
+05 Activity_behaviour
+06 Human_activity
+07 Presence_richness
+08 Managment
+09 Prey-predator
+%
+
+nation_park       01
+protect_area      01
+reserve           01
+hot_spot          01
+conserv_strategi  01
+endeng_species    01
+
+
+large_carnivor    02
+large_mammal      02
+red_fox           02
+larg_mammal       02
+larg_carnivor     02
+snow_leopard      02
+wild_boar         02
+
+densiti_estim     03
+popul_densiti     03
+estim_densiti     03
+
+detect_probabl    04
+occup_model       04
+abunda_estim      04
+
+
+activ_pattern     05
+daili_activ       05
+nocturn_activ     05
+forest_ecol       05
+feed_eco          05
+nesting_behav     05
+seed_disp         05
+
+
+
+human_activ       06
+habitat_loss      06
+hab_fragment      06
+hab_loss          06
+fire              06
+road_effect       06
+human_conflict    06
+hunt              06
+pouching          06 
+logg              06
+
+monitoring        07
+rare_species      07
+survey_           07
+inventory         07
+presen_spec       07
+mammal_com
+
+
+
+hybrid            08
+invasion          08
+green_pass        08
+pest_control      08
+urban_zone        08
+tourism           08
+disease_trans     08
+citizen_scien     08
+legislacy         08
+reintrodu         08
+
+
+prey_             09
+predat_
+scaveng_
+predat_risk
+
+
+
+
+
