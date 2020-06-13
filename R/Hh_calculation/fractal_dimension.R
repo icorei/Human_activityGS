57b3badd6c52d13c2ebed01952089f0573907ea3
icorei <42251398+icorei@users.noreply.github.com>
2020-06-13 12:59:14 +0200

    Update fractal dimension file
    Inclui el calculo segun Gneiting et al. (2012). Sin emargo igual no me corre. Puedes porfa @jrfep revisalo?

--- R/Hh_calculation/fractal_dimension.R
+++ R/Hh_calculation/fractal_dimension.R
@@ -56,6 +56,52 @@ for (k in 7:10){
   
 }
 
+## ALTERNATIVA ##Funcion para calcular diemension fractal dentro del bloque segun Gneiting 2012.
+####
+
+#patch
+#k=1
+#crop(vbsq,extent(subset(grd,Id==k)))
+
+#ClassStat(ptch, latlon=TRUE)[2,"mean.frac.dim.index"]                   
+
+fds <- hs <- c()
+for (k in 7:10){
+  ptch <- crop(vbsq,extent(subset(grd,Id==k)),
+               snap="near")>40
+  
+  
+  hs <- c(hs,sum(values(ptch),na.rm=T)/ncell(ptch))
+  fds <- c(fds,ClassStat(ptch, latlon=TRUE)[2,"perimeter.area.frac.dim"])
+  
+  plot(hs,fds)
+  
+}
+
+fds <- hs <- c()
+for (Id in 1:6){
+  ptch <- crop(vbsq,extent(subset(grd,cuadrado==Id)),
+               snap="near")
+  if (nrow(ptch)!=ncol(ptch))
+    ptch <- crop(vbsq,extent(subset(grd,cuadrado==k)),
+                 snap="out")
+  
+  hs <- c(hs,sum(values(ptch),na.rm=T)/ncell(ptch))
+  mat <- matrix(values(ptch),ncol=ncol(ptch),byrow=T)+0
+  if (nrow(ptch)==ncol(ptch)) {
+    fds <- c(fds,(fd.estim.filter1(mat))$fd)
+  } else {
+    if (nrow(mat)>ncol(mat))
+      mat <- mat[-1,]
+    if (ncol(mat)>nrow(mat))
+      mat <- mat[,-1]
+    fds <- c(fds,(fd.estim.filter1(mat))$fd)
+  }
+  plot(hs,fds)
+  
+}
+
+
 ##mapa del area de estudio con diseño de cuadrados seleccionados
 ## h: proporcion de bosque en el paisaje
 ## H: dimension fractal del bosque en el paisaje
