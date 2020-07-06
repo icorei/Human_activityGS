9c59edfe8cbca001673d8d31a20356875da2d1e1
jrfep <jr.ferrer.paris@gmail.com>
2020-07-06 16:06:48 +1000

    updated models for all species

--- R/Occupancy_ models/single_visit_models.R
+++ R/Occupancy_ models/single_visit_models.R
@@ -51,34 +51,114 @@ eventos %>% mutate(target=species %in% "D.kappleri") %>% group_by(grid) %>% summ
 eventos %>% mutate(target=species %in% "E.barbara") %>% group_by(grid) %>% summarise(pa=max(target)) -> species
 
 for (sp in levels(eventos$species)) {
-  eventos %>% mutate(target=species %in% sp) %>% group_by(grid) %>% summarise(pa=max(target)) -> species
+   eventos %>% mutate(target=species %in% sp) %>% group_by(grid) %>% summarise(pa=max(target)) -> species
 
 
-  dts  %>% left_join(walk) %>% left_join(cams) %>% left_join(species) %>% filter(!is.na(walk) | !is.na(cam)) %>% transmute(grid, walk, cam, caz=as.factor(!caz %in% 0), pa,
-      bsq=(bsq-mean(bsq))/sd(bsq),
-      dst=(dst-mean(dst,na.rm=T))/sd(dst,na.rm=T),
-      frs=(frs-mean(frs,na.rm=T))/sd(frs,na.rm=T),
-      dbsq=(dbsq-mean(dbsq,na.rm=T))/sd(dbsq,na.rm=T),
-    ) -> pa.data
+   dts  %>% left_join(walk) %>% left_join(cams) %>% left_join(species) %>% filter(!is.na(walk) | !is.na(cam)) %>% transmute(grid, walk, cam, caz=as.factor(!caz %in% 0), pa,
+   bsq=(bsq-mean(bsq))/sd(bsq),
+   dst=(dst-mean(dst,na.rm=T))/sd(dst,na.rm=T),
+   frs=(frs-mean(frs,na.rm=T))/sd(frs,na.rm=T),
+   dbsq=(dbsq-mean(dbsq,na.rm=T))/sd(dbsq,na.rm=T)) -> pa.data
 
-  pa.data$walk <- coalesce(pa.data$walk,min(walk$walk,na.rm=T))
-  pa.data$cam <- coalesce(pa.data$cam,min(cams$cam,na.rm=T))
-  pa.data$dst <- coalesce(pa.data$dst,max(pa.data$dst,na.rm=T))
-  pa.data$dbsq <- coalesce(pa.data$dbsq,max(pa.data$dbsq,na.rm=T))
-  pa.data$frs <- coalesce(pa.data$frs,max(pa.data$frs,na.rm=T))
-  pa.data$pa <- coalesce(pa.data$pa,0L)
-##  pa.data$caz <- coalesce(pa.data$caz,0L)
+   pa.data$walk <- coalesce(pa.data$walk,min(walk$walk,na.rm=T))
+   pa.data$cam <- coalesce(pa.data$cam,min(cams$cam,na.rm=T))
+   pa.data$dst <- coalesce(pa.data$dst,max(pa.data$dst,na.rm=T))
+   pa.data$dbsq <- coalesce(pa.data$dbsq,max(pa.data$dbsq,na.rm=T))
+   pa.data$frs <- coalesce(pa.data$frs,max(pa.data$frs,na.rm=T))
+   pa.data$pa <- coalesce(pa.data$pa,0L)
+   ##  pa.data$caz <- coalesce(pa.data$caz,0L)
 
 
-  fit <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+   pre <- fit <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
 
-  assign(sprintf("%s.full",sp),fit)
+   if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+      assign(sprintf("%s.full",sp),fit)
+   } else {
+      fit <- svocc(pa ~ bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+   }
 
-  sfit <- svocc.step(fit, model="sta")
+   if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+      assign(sprintf("%s.full",sp),fit)
+   } else {
+      fit <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk*cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+   }
+   if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+      assign(sprintf("%s.full",sp),fit)
+   } else {
+      fit <- svocc(pa ~ bsq + dst + frs + dbsq| walk*cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+   }
 
-  assign(sprintf("%s.step",sp),sfit)
+   if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+      assign(sprintf("%s.full",sp),fit)
+   } else {
+      fit <- svocc.step(pre, model="sta")
+      if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<4) ) {
+         assign(sprintf("%s.step",sp),fit)
+      } else {
+         pre <- svocc(pa ~ bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+         fit <- svocc.step(pre, model="sta")
+      }
 
+      if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<4) ) {
+         assign(sprintf("%s.step",sp),fit)
+      } else {
+         pre <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk*cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+         fit <- svocc.step(pre, model="sta")
+      }
+      if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+         assign(sprintf("%s.step",sp),fit)
+      } else {
+         pre <- svocc(pa ~ bsq + dst + frs + dbsq| walk*cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+         fit <- svocc.step(pre, model="sta")
+      }
+
+      if (all(abs(fit$coefficients$sta)<10) & all(!is.na(fit$std.error$sta)) & all(fit$std.error$sta<6) ) {
+         assign(sprintf("%s.step",sp),fit)
+      }
+   }
 }
+
+## al menos 10-13 species con modelos decentess
+ls(pattern=".full")
+ls(pattern=".step")
+
+Listos
+           Var1 Freq
+1          C.paca  344
+2      D.leporina  236
+3         C.thous   94
+4   M.gouazoubira   69
+5      D.kappleri   56
+6  D.novemcinctus   47
+7    T.terrestris   35
+8      P.concolor   32
+10         P.onca   27
+12   M.tridactyla   23
+16      P.maximus    9
+19 T.tetradactyla    6
+20  O.virginianus    5
+
+Faltan
+           Var1 Freq
+9     M.americana   32
+11     L.pardalis   24
+13      E.barbara   22
+14   D.imperfecta   14
+15  H.hydrohaeris   10
+17        N.nasua    8
+18    C.olivaceus    8
+21       P.tajacu    4
+22   C.unicinctus    4
+23  D.marsupialis    3
+24       T.pecari    2
+25       M.pratti    2
+26       L.wiedii    2
+27    S.venaticus    1
+28     L.tigrinus    1
+
+
+
+assign(sprintf("%s.step",sp),sfit)
 
 sprintf("summary(%s.full)\n",levels(eventos$species))
 
@@ -91,13 +171,16 @@ summary(M.tridactyla.full)
 summary(O.virginianus.full)
 summary(T.tetradactyla.full)
 
+confint(C.paca.full,"sta_bsq")
+confint(C.thous.full)
+
 ## good sta, bad det
 
 summary(P.maximus.step)
 
 
 
-for (sp in c("P.maximus","C.olivaceus","C.unicinctus", "D.imperfecta", "D.leporina", "D.marsupialis", "D.novemcinctus", "E.barbara", "H.hydrohaeris", "L.pardalis", "L.tigrinus", "L.wiedii", "M.americana", "M.pratti", "N.nasua", "P.concolor", "P.onca", "P.tajacu", "S.venaticus","T.terrestris", "T.pecari")) {
+for (sp in c( "P.concolor", "P.onca")) {
   eventos %>% mutate(target=species %in% sp) %>% group_by(grid) %>% summarise(pa=max(target)) -> species
 
 
@@ -131,30 +214,80 @@ for (sp in c("P.maximus","C.olivaceus","C.unicinctus", "D.imperfecta", "D.lepori
 }
 
 
-summary(C.olivaceus.step)
- summary(C.unicinctus.step)
- summary(D.imperfecta.step)
- summary(D.leporina.step)
- summary(D.marsupialis.step)
- summary(D.novemcinctus.step)
- summary(E.barbara.step)
- summary(H.hydrohaeris.step)
- summary(L.pardalis.step)
- summary(L.tigrinus.step)
- summary(L.wiedii.step)
- summary(M.americana.step)
- summary(M.pratti.step)
-
- summary(N.nasua.step)
- summary(P.concolor.step)
+## good sta, bad det
+ summary(P.onca.step.pen)
+ summary(P.concolor.step.pen)
 
 
- summary(P.onca.step)
- summary(P.tajacu.step)
- summary(S.venaticus.step)
- summary(T.terrestris.step)
- # not converged
- summary(T.pecari.step)
+
+for (sp in c( "D.leporina", "P.maximus","C.olivaceus","C.unicinctus", "D.imperfecta",  "D.marsupialis", "D.novemcinctus", "E.barbara", "H.hydrohaeris", "L.pardalis", "L.tigrinus", "L.wiedii", "M.americana", "M.pratti", "N.nasua","P.tajacu", "S.venaticus","T.terrestris", "T.pecari")) {
+  eventos %>% mutate(target=species %in% sp) %>% group_by(grid) %>% summarise(pa=max(target)) -> species
+
+
+  dts  %>% left_join(walk) %>% left_join(cams) %>% left_join(species) %>% filter(!is.na(walk) | !is.na(cam)) %>% transmute(grid, walk, cam, caz=as.factor(!caz %in% 0), pa,
+      bsq=(bsq-mean(bsq))/sd(bsq),
+      dst=(dst-mean(dst,na.rm=T))/sd(dst,na.rm=T),
+      frs=(frs-mean(frs,na.rm=T))/sd(frs,na.rm=T),
+      dbsq=(dbsq-mean(dbsq,na.rm=T))/sd(dbsq,na.rm=T),
+    ) -> pa.data
+
+  pa.data$walk <- coalesce(pa.data$walk,min(walk$walk,na.rm=T))
+  pa.data$cam <- coalesce(pa.data$cam,min(cams$cam,na.rm=T))
+  pa.data$dst <- coalesce(pa.data$dst,max(pa.data$dst,na.rm=T))
+  pa.data$dbsq <- coalesce(pa.data$dbsq,max(pa.data$dbsq,na.rm=T))
+  pa.data$frs <- coalesce(pa.data$frs,max(pa.data$frs,na.rm=T))
+  pa.data$pa <- coalesce(pa.data$pa,0L)
+##  pa.data$caz <- coalesce(pa.data$caz,0L)
+
+## potential changes to explore:
+## change link.sta to probit or logit
+## use walk*cam instead of walk+cam in detectability formula
+## discard the caz covariate
+
+  fit <- svocc(pa ~ bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+sfit <- svocc.step(fit, model="sta")
+
+fit <- svocc(pa ~ bsq + dst + frs + dbsq| walk*cam, data=pa.data, link.sta = "cloglog", link.det = "logit", penalized = FALSE, method = c( "optim"))
+sfit <- svocc.step(fit, model="sta")
+
+  fit <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "probit", link.det = "logit", penalized = TRUE, method = c( "optim"))
+
+    fit <- svocc(pa ~ caz + bsq + dst + frs + dbsq| walk+cam, data=pa.data, link.sta = "logit", link.det = "logit", penalized = TRUE, method = c( "optim"))
+  bfit <- bootstrap(fit, B=50)
+
+  assign(sprintf("%s.full.pen",sp),bfit)
+
+
+  sfit <- svocc.step(bfit, model="sta")
+  sbfit <- bootstrap(sfit, B=50)
+
+  assign(sprintf("%s.step.pen",sp),sbfit)
+
+}
+
+ summary(H.hydrohaeris.full.pen)
+ summary(L.pardalis.full.pen)
+ summary(L.tigrinus.full.pen)
+ summary(L.wiedii.full.pen)
+ summary(M.americana.full.pen)
+ summary(M.pratti.full.pen)
+
+ summary(N.nasua.full.pen)
+
+
+ summary(S.venaticus.full.pen)
+ # not working
+ summary(T.pecari.full.pen)
+ summary(T.terrestris.full.pen)
+ summary(P.tajacu.full.pen)
+ summary(C.olivaceus.full.pen)
+  summary(C.unicinctus.full.pen)
+  summary(D.imperfecta.full.pen)
+  summary(D.leporina.full.pen)
+  summary(D.marsupialis.full.pen)
+  summary(D.novemcinctus.full.pen)
+  summary(E.barbara.full.pen)
+
 
 summary(bfit,"mle")
 summary(bfit,"pmle")
