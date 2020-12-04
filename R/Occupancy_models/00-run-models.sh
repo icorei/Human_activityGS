760c624373e59d4f8c5b659bf0551f9893eb2eb6
jrfep <jr.ferrer.paris@gmail.com>
2020-12-04 12:08:20 +1100

    Summary of results of unmarked models

--- R/Occupancy_models/00-run-models.sh
+++ R/Occupancy_models/00-run-models.sh
@@ -4,6 +4,13 @@ ls -lah $SCRIPTDIR/Rdata
 
 nohup Rscript --vanilla $SCRIPTDIR/R/Occupancy_models/occuRN-models.R &
 
+
+
+require(knitr)
+mi.arch <- sprintf("%s/R/Occupancy_models/unmarked-models.Rmd",script.dir)
+rmarkdown::render(mi.arch,"all")
+
+
 for SPECIE in C.alector C.olivaceus C.paca C.thous C.unicinctus D.imperfecta D.kappleri D.leporina D.marsupialis D.novemcinctus E.barbara H.hydrochaeris L.pardalis L.rufaxilla L.tigrinus L.wiedii M.americana M.gouazoubira M.pratti M.tridactyla N.nasua O.virginianus P.concolor P.maximus P.onca P.tajacu S.venaticus T.major T.pecari T.terrestris T.tetradactyla
 do
    nohup Rscript --vanilla $SCRIPTDIR/R/Occupancy_models/presence-absence-dataframe.R $SPECIE &
