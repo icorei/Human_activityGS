8955bd3d5e3e031607d8cb4fd9dccbd495fc229a
jrfep <jr.ferrer.paris@gmail.com>
2021-01-26 22:11:31 +1100

    Update draft of suppl. methods (part I and part II)

--- R/Occupancy_models/00-run-models.sh
+++ R/Occupancy_models/00-run-models.sh
@@ -11,8 +11,8 @@ cd $WORKDIR
 R --vanilla
 require(knitr)
 script.dir <- Sys.getenv("SCRIPTDIR")
-mi.arch <- sprintf("%s/R/Occupancy_models/unmarked-models.Rmd",script.dir)
-mi.arch <- sprintf("%s/R/Occupancy_models/supplementary-methods.Rmd",script.dir)
+mi.arch <- sprintf("%s/R/Occupancy_models/supplementary-methods-2.Rmd",script.dir)
+mi.arch <- sprintf("%s/R/Occupancy_models/supplementary-methods-1.Rmd",script.dir)
 knitr::opts_chunk$set(warning = FALSE, echo = TRUE, eval = TRUE)
 
 rmarkdown::render(mi.arch,"all")
